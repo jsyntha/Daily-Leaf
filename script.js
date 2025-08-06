af26c8d9ef3e393d60470e5d00ec028e16ee32ce
jsyntha <acipher65@gmail.com>
2025-08-06 22:41:00 +0100

    Added dragging foundation

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ iconButtons.forEach(button => {
     }
   });
 });
+
+// icon dragging 
+document.querySelectorAll(".toolbar-item").forEach(item => {
+    const icon = item.querySelector(".icon");
+    let isDragging = false;
+    let offsetX = 0;
+    let offsetY = 0;
+
+    icon.addEventListener("mousedown", (e) => {
+        isDragging = true;
+
+        const rect = item.getBoundingClientRect();
+        offsetX = e.clientX - rect.left;
+        offsetY = e.clientY - rect.top;
+
+        item.style.zIndex = 1000;
+    });
+
+    document.addEventListener("mousemove", (e) => {
+        if (!isDragging) return;
+        item.style.left = (e.clientX - offsetX) + "px";
+        item.style.top = (e.clientY - offsetY) + "px";
+    });
+
+    document.addEventListener("mouseup", () => {
+        isDragging = false;
+    });
+});
